77d43f5aade7bf905ae966def43297d1cb88e18f
Lorena <lorenaborgessilva@gmail.com>
2023-12-05 22:35:27 -0300

    ajuste statuscode

--- src/controllers/UsersController.ts
+++ src/controllers/UsersController.ts
@@ -364,7 +364,7 @@ export class UsersController {
     const token = req.body.authToken;
     try {
       await new TokenService().removeToken(token);
-      return res.status(httpCodes.OK).send();
+      return res.status(httpCodes.NO_CONTENT).send();
     } catch (error) {
       return res.status(httpCodes.BAD_REQUEST).json(error);
     }
