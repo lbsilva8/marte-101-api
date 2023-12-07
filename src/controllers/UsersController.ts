5a42378a103d41b16e058d64d1d00590a9b5bb33
MartinsNeto80 <martinsneto.8080@gmail.com>
2023-12-07 10:27:48 -0300

    Correção de PR

--- src/controllers/UsersController.ts
+++ src/controllers/UsersController.ts
@@ -148,7 +148,9 @@ export class UsersController {
     } catch (error) {
       const route: string = '/users/recover-password: ';
       await new ErrorLogService().insertError(error, route);
-      return res.status(httpCodes.BAD_REQUEST).json(error);
+      return res
+        .status(httpCodes.BAD_REQUEST)
+        .json({ error: { message: error.message } });
     }
   }
 
